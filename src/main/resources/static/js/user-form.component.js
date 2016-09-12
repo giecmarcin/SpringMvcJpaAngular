1f1187f3ba72d3abdeed6779cb98bb7a6d4952df
giecmarcin <giecmarcin@gmail.com>
2016-09-12 15:58:56 +0200

    //

--- src/main/resources/static/js/user-form.component.js
+++ src/main/resources/static/js/user-form.component.js
@@ -1,4 +1,12 @@
+var UserFormController = function($scope) {
+    $scope.contacts = [];
+    $scope.addContact = function () {
+        $scope.contacts.push({})
+    }
+};
+
 angular.module('userApp').component('userForm', {
     templateUrl: 'js/user-form.template.html',
+    controller: UserFormController
 })
 ;
